1a3a4b2d1e0e039969a7a45fe46afdab3ec45a92
최윤지 <ChoiYunji@choeyunjiui-MacBook.local>
2019-07-31 16:52:55 +0900

    first commit

--- secondproject/settings.py
+++ secondproject/settings.py
@@ -136,6 +136,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 # Heroku: Update database configuration from $DATABASE_URL.
-    import dj_database_url
-    db_from_env = dj_database_url.config(conn_max_age=500)
-    DATABASES['default'].update(db_from_env)
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
